1d74947ecd2ae15cdc69c49af90ed4b4745b64a9
Fishball Yuen <oxfishballxo@gmail.com>
2021-07-20 01:27:14 +0800

    final version with excel export

--- ManningsPricingScraping.py
+++ ManningsPricingScraping.py
@@ -4,17 +4,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+import pandas as pd
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-url = "https://www.mannings.com.hk/personalcarenhair/personal-care/sanitary-protection/c/fesanitaryprotection"
+#url = "https://www.mannings.com.hk/personalcarenhair/personal-care/sanitary-protection/c/fesanitaryprotection"
+
+url = input("what do you want to check?Please start from the first page!")
 
 r = driver.get(url)
 
 # driver.maximize_window()
 
 try:  # popup message handler
-    close_popup = WebDriverWait(driver, 1).until(
+    close_popup = WebDriverWait(driver, 5).until(
         EC.element_to_be_clickable((By.ID, "pdv4close"))).click()
 except:
     print("no popup message!")
@@ -22,6 +25,7 @@ except:
 product_offer_list = []
 product_price_list = []
 product_list = []
+product_id_list = []
 condition = True
 while condition:
     productInfoList = driver.find_elements_by_class_name("product_content")
@@ -32,15 +36,18 @@ while condition:
             "//div[@class='offer_section']/span[@class='hidden-xs']")
     for product in products:
         product_list.append(product.text)
-    for product_price in product_prices:
-        product_price_list.append(product_price.text)
-    for product_offer in product_offers:
-        product_offer_list.append(product_offer.text)
+    for pp in product_prices:
+        product_price_list.append(pp.text)
+    for po in product_offers:
+        product_offer_list.append(po.text)
     try:
         driver.find_element_by_xpath(
             "//li[@class='pagination-next']/a").click()  # go to next page
     except:
         condition = False
+print(product_id_list)
 
 pricing = list(zip(product_list, product_price_list, product_offer_list))
 print(pricing)
+data = pd.DataFrame(pricing)
+data.to_excel("pricing_data.xlsx", index=False)
